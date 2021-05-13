b017c25aaaafb1259bfcf9446f3f3fd880cc4ed8
Xavster838 <francisguitart@gmail.com>
2021-05-13 14:41:30 -0700

    moved all config processing functions to scripts/smk_helper_funcs.py. Still not written, Just function descriptions.

--- pull_gff_annotated_sequence.smk
+++ pull_gff_annotated_sequence.smk
@@ -11,14 +11,3 @@ CNF_DIR = os.path.dirname( os.path.abspath(config_file) ) #dir holding config fi
 SCRIPT_DIR = SMK_DIR + "/scripts"
 
 exec(open(SCRIPT_DIR + smk_helper_func ).read()) ##TO DO: FIGURE THIS OUT!
-#######################		Process Yaml
-def process_config(SM):
-	'''figure out file info, gene name, specific queries to sample, etc. all from config'''
-	return( data_structs_from_config )
-
-
-def get_rule_all_input_files(wc):
-	'''return file names rule_all will want given process_config() results from config.yaml'''
-
- 
-#####################################################################
--- scripts/smk_helper_funcs.py
+++ scripts/smk_helper_funcs.py
@@ -1,1 +1,21 @@
 #helper functions for snakemake file.
+
+#######################		Process Yaml
+def process_config(SM):
+	'''figure out file info, gene name, specific queries to sample, etc. all from config'''
+	return( data_structs_from_config )
+
+
+def get_rule_all_input_files(wc):
+	'''return file names rule_all will want given process_config() results from config.yaml'''
+
+def process_seq_pull_list(wc):
+    '''figure out sequence user wants given sequence list in config.yaml. default is full sequence if nothing returned.'''
+
+def process_queries(wc):
+    '''if query info passed, figure out contigs in gff and fasta file corresponding to query information given. Return dictionary'''
+
+def process_table(wc):
+    '''if table passed, use it to map sample names to contigs in gff and fasta files. Return dictionary'''
+ 
+#####################################################################
